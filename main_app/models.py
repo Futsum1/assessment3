dd02fd26b58ad9e9acbe8efc14b389e9245e3f18
Futsum Nemariam <futsumnemariam2@gmil.com>
2020-09-25 12:39:25 -0700

    add widgets

--- main_app/models.py
+++ main_app/models.py
@@ -3,8 +3,9 @@ from django.urls import reverse
 
 # Create your models here.
 class Todo(models.Model):
-    item = models.CharField(max_length=50)
-
+    description = models.CharField(max_length=50)
+    quantity = models.CharField(max_length=50)
+   
     def __str__(self):
         return self.item
 
